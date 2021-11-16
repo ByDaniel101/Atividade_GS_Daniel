7db568111a512eada57e8bd4e664a33f32979fff
Daniel de Moura Santos <danielmsa101@gmail.com>
2021-11-15 23:06:56 -0300

    Classe Daniel Criada
    Classe Daniel com a função de soma

---
+++ atividadeGsDaniel/src/classes/Daniel.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class Daniel {
+	private int numero1, numero2, numero3;
+	
+	public Daniel() {
+		
+	}
+	
+	public void somar(int n1,int n2) {
+		
+		numero1 = n1;
+		numero2 = n2;
+		numero3 = numero1 + numero2;
+	}
+}
